1a313f267d910434045283b943656e54b82331ac
Fabrice Ralph Ndui Ntoutoume <fandua@l4714-04.info.polymtl.ca>
2019-01-14 14:39:21 -0500

    added interrupteur, still have issue in logic

--- compteur32bits.cpp
+++ compteur32bits.cpp
@@ -11,13 +11,27 @@
 #include <util/delay.h>
 //#define F_CPU 14.7456E6;
 
+void antirebond(unsigned int& decompte, bool& pressed){
+  if( PIND & 0x04 ){
+    if(decompte < 2){decompte++;}
+    
+    if(decompte == 2){
+      pressed = false;
+    }
+  }else{
+    decompte = 0;
+    pressed = true;
+  }
+}
+
 int main()
 {
   DDRA = 0xff; // PORT A est en mode sortie
   DDRB = 0xff; // PORT B est en mode sortie
   DDRC = 0xff; // PORT C est en mode sortie
-  DDRD = 0xff; // PORT D est en mode sortie
-  unsigned long compteu=0;
+  DDRD = 0x00; // PORT D est en mode lecture
+  unsigned int decompte = 0;
+  bool pressed = false;
   unsigned long compteur=0; // le compteur est initialise a 0.
                             // c'est un compteur de 32 bits
 
@@ -29,24 +43,16 @@ int main()
                             // il prend les bits de 0 a 7 du compteur
     PORTC = compteur >> 8;  // PORTC = 8 bits de large,
     
-    
+    antirebond(decompte,pressed);
 
-
-    PORTB = 0x01;    
-     _delay_ms(3150);                   // il prend les bits de 8 a 15 du compteur
-
-                            // en faisant un decallage de 8 bits.
-    for(int i=0; i< 30;i++){
-    PORTB = 0x02; // PORTB = 8 bits de large,
-    _delay_ms(100);
-    // PORTB = 0x5a;
-    PORTB = 0x01;
-     _delay_ms(10);
-     }
-
-    PORTB = 0x02; 
-     _delay_ms(3150);
-    
+    if(pressed){
+      PORTB = 0x02; // PORTB = 8 bits de large,
+      _delay_ms(100);
+      PORTB = 0x01;
+      _delay_ms(10);
+    }else{
+      PORTB = 0x00;
+    }
     
                             // il prend les bits de 16 a 23 du compteur
                             // en faisant un decallage de 16 bits.
